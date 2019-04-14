23ad629699ad59c066f69ca9495ab9d641a9d3b3
Kaitlyn Middlebrook <kmiddlebrook2000@gmail.com>
2019-04-14 18:31:56 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,11 +1,11 @@
 WIN_COMBINATIONS = board[0] == board[1] == board[2];
-board[3] == board[4] == board[5]
-board[6] == board[7] == board[8]
-board[0] == board[3] == board[6]
-board[1] == board[4] == board[7]
-board[2] == board[5] == board[8]
-board[0] == board[4] == board[8]
-board[6] == board[4] == board[2]
+board[3] == board[4] == board[5];
+board[6] == board[7] == board[8];
+board[0] == board[3] == board[6];
+board[1] == board[4] == board[7];
+board[2] == board[5] == board[8];
+board[0] == board[4] == board[8];
+board[6] == board[4] == board[2];
 
 def display_board
   
